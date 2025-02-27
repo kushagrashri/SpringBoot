47ef0ac37132268e66c1df93f29af9ff266aa1a4
kushagrashri7 <kushagrashrisharma@gmail.com>
2025-02-27 11:49:44 +0530

    [Kushagra] Add.SpringIntro UC_03

--- src/main/java/com/app/hello/HelloController.java
+++ src/main/java/com/app/hello/HelloController.java
@@ -1,9 +1,6 @@
 package com.app.hello;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/hello")
@@ -19,6 +16,12 @@ public class HelloController {
     //UC-2
     @GetMapping("/hello2")
     public String sayHello2(@RequestParam String name){
+        return "Hello " + name + " from BridgeLabz";
+    }
+
+    //UC-3
+    @GetMapping("/hello3/{name}")
+    public String sayHello3(@PathVariable String name){
         return "Hello " + name + " from BridgeLabz";
     }
 
